671ddb1527310adf8c2849d54af9ca237de6262d
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-09-28 09:34:57 -1000

    resolves all tests

--- generator.js
+++ generator.js
@@ -1,3 +1,14 @@
 module.exports = function(n) {
-  // do work here
+  // generate an array of length n with numerals 0 - (n-1)
+  let array = [];
+  for (let i = 0; i < n; i++) {
+    array.push(i);
+  }
+
+  // pluck out random elements and put them into a new array
+  let result = [];
+  while (array.length > 0) {
+    result.push(array.splice(Math.floor(Math.random() * array.length), 1)[0]);
+  }
+  return result;
 };
