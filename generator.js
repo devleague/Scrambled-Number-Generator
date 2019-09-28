d53c7af0e96d94714edcecbcbad2f4bd3f5781d7
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-09-28 10:02:51 -1000

    complete challenge

--- generator.js
+++ generator.js
@@ -1,3 +1,15 @@
 module.exports = function(n) {
   // do work here
+  let arr = [];
+  for (let i = 1; i < n; i++) {
+    arr.push(i);
+  }
+
+  function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+
+  shuffle(arr);
+  arr.unshift(0);
+  return arr;
 };
